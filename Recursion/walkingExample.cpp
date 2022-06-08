85c3ee5be2c9fa037756fd28aeaa92e7422a84b7
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-06-08 21:55:43 +0530

    Learn basic of recursion

---
+++ Recursion/walkingExample.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void reachHome(int src,int dest){
+
+    // base case
+    if(src == dest){
+        cout<<"Reached home"<<endl;
+        return ;
+    }
+
+    // processing :- we know one by one step we reach dest from src
+    src++;
+
+    //recursive call 
+    reachHome(src,dest);
+}
+
+int main(){
+    int src = 1;
+    int dest = 10;
+
+    reachHome(src,dest);
+}
