ab30500d1c4b7d0f25d641634b559f732225c67d
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-06-09 12:17:57 +0530

    optimize the problem at palace of two pointer we use only one pointer here

--- Recursion/checkPalindrome.cpp
+++ Recursion/checkPalindrome.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
 using namespace std;
 
-bool checkPalindrome(string str, int i, int j)
+// bool checkPalindrome(string str, int i, int j)
+// {
+
+//     // base case
+//     if (i > j)
+//     {
+//         return true;
+//     }
+
+//     if (str[i] != str[j])
+//     {
+//         return false;
+//     }
+//     else
+//     {
+//         return checkPalindrome(str, i + 1, j - 1);
+//     }
+// }
+
+// optimize code by using single pointer
+
+bool checkPalindrome(string str, int i)
 {
 
     // base case
-    if (i > j)
+    if (i > str.length()-1-i)
     {
         return true;
     }
 
-    if (str[i] != str[j])
+    if (str[i] != str[str.length()-1-i])
     {
         return false;
     }
     else
     {
-        return checkPalindrome(str, i + 1, j - 1);
+        return checkPalindrome(str, i + 1);
     }
 }
 
 int main()
 {
-    string str = "abba";
+    string str = "abbaa";
 
-    bool isPalindrome = checkPalindrome(str, 0, str.length() - 1);
+    // bool isPalindrome = checkPalindrome(str, 0, str.length() - 1);
+
+    // optimize the code by using single pointer
+    bool isPalindrome = checkPalindrome(str, 0);
 
     cout << endl;
     if (isPalindrome)
