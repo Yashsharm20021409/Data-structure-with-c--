cea8e30c11bdd142c49d893f6866d325c09f13df
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2022-06-17 01:10:26 +0530

    update

--- DP/0/KnapSackUsingTabulationMethod.cpp
+++ DP/0/KnapSackUsingTabulationMethod.cpp
@@ -1,29 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int main()
+{
+    int profit[5] = {0, 1, 2, 3, 4};
+    int weight[5] = {0, 2, 3, 4, 5};
 
-int main(){
-    int profit[5] = {0,1,2,3,4};
-    int weight[5] = {0,2,3,4,5};
-
-    int m = 8; //knapsack size
+    int m = 8; // knapsack size
     int n = 4; // number of object
 
     int knap[5][9];
 
-
-    for(int i = 0 ;i<=n ; i++){
-        for(int w = 0 ; w<= m ; w++){
-            if(i == 0 || w == 0){
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= m; w++)
+        {
+            if (i == 0 || w == 0)
+            {
                 knap[i][w] = 0;
             }
-            else if(weight[i] <= w){
-                knap[i][w] = max(profit[i]+ knap[i-1][w-weight[i]],knap[i-1][w]);
+            else if (weight[i] <= w)
+            {
+                knap[i][w] = max(profit[i] + knap[i - 1][w - weight[i]], knap[i - 1][w]);
             }
-            else{
-                knap[i][w] = knap[i-1][w];
+            else
+            {
+                knap[i][w] = knap[i - 1][w];
             }
         }
     }
-    cout<<knap[n][m]<<endl;
+    cout << knap[n][m] << endl;
+
+    int i = n,j = m;
+
+    // to check which object is include which note
+    while (i > 0 && j > 0)
+    {
+
+        if (knap[i][j] == knap[i - 1][j])
+        {
+            cout << i << " = 0" << endl;
+            i--;
+        }
+        else
+        {
+            cout << i << " = 1" << endl;
+            j = j - weight[i];
+            i--;
+        }
+    }
 }
